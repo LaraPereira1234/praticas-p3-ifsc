bd63bb071d099120d8f66b5f658847d76ebc5634
Lara <lara.p2006@aluno.ifsc.edu.br>
2023-05-17 14:41:06 -0300

    lista3

---
+++ lista3/src/exc10/Animal.java
@@ -0,0 +1,11 @@
+package exc10;
+
+public class Animal {
+
+	public String nomeC;
+	public String famila;
+	public String reino;
+	public String[] nomesPopulares;
+	
+
+}
---
+++ lista3/src/exc10/mainc.java
@@ -0,0 +1,33 @@
+package exc10;
+
+public class mainc {
+	
+	public static void main(String[] args) {
+		Animal al = new Animal();
+		al.nomeC = "SerinosCanaria";
+		al.famila = "Fringillidae";
+		al.reino = "animal";
+		
+		String[]  nomePopular = new String[5];
+		nomePopular[0] = "canario-belga";
+		nomePopular[1] = "canario-do-reino";
+		nomePopular[2] = "canario-imperio";
+		nomePopular[3] = "canario-domestico";
+		nomePopular[4] = "canario";
+		
+		al.nomesPopulares = nomePopular;
+		
+		
+				
+				
+				
+				
+				
+				
+				
+	    
+		
+		
+	}
+
+}
